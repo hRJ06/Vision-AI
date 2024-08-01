533d4fdafb465d735e8b8c9a7a5a5c1eca292728
hRJ06 <hindolroy.2306@gmail.com>
2024-08-01 23:49:47 +0530

    Fixed Mail Server Action

--- Client/lib/actions/mail.action.ts
+++ Client/lib/actions/mail.action.ts
@@ -6,7 +6,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export const sendEmail = async ({ name, email }: Mail) => {
   try {
     const { data, error } = await resend.emails.send({
-      from: "Vision AI <noreply@vision-ai.in>",
+      from: "noreply@vision.ai <noreply@vision-ai.in>",
       to: email,
       subject: "Thank You For Choosing Vision AI",
       react: EmailTemplate({ firstName: name }),
